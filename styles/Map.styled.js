9cf0f7b64bdc752b20da068e2e01ef3b33b38d7f
jayesh2906 <jayesh.choudhary2906@gmail.com>
2022-07-21 12:52:16 +0530

    search not found

--- styles/Map.styled.js
+++ styles/Map.styled.js
@@ -4,12 +4,13 @@ export const MapContainer = styled.main`
   margin-top: 2rem;
   padding: 0 calc(2rem);
   background: url(../assets/loader.gif) center center no-repeat;
-  background-size: 10%;
+  background-size: 7%;
   @media ${(props) => props.theme.breakpoints.lg} {
     padding: 0 calc(2rem);
   }
   @media ${(props) => props.theme.breakpoints.md} {
     padding: 0px;
+    background-size: 10%;
   }
   @media ${(props) => props.theme.breakpoints.sm} {
     background-size: 20%;
